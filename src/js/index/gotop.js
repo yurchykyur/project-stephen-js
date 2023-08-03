a6cda6811a02491cfed5bc0e0e01f0882c9e3e28
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-08-03 14:49:12 +0300

    add go top btn

--- src/js/index/gotop.js
+++ src/js/index/gotop.js
@@ -1,22 +1,14 @@
-// Отримуємо елемент "gotop-wrapper"
 const goToTopWrapper = document.querySelector('.gotop-wrapper');
 
-// Отримуємо кнопку "Go to top"
 const goToTopBtn = document.querySelector('.gototop');
 
-// Додаємо прослуховувач подій для визначення положення користувача на сторінці
 window.addEventListener('scroll', () => {
-  // Визначаємо положення користувача на сторінці
-  const currentPosition =
-    window.pageYOffset || document.documentElement.scrollTop;
+  const currentPosition = document.documentElement.scrollTop;
 
-  // Визначаємо висоту вмісту сторінки
   const pageHeight = document.documentElement.scrollHeight;
 
-  // Визначаємо висоту вікна браузера
   const windowHeight = window.innerHeight;
 
-  // Якщо користувач дійшов до певної відстані від кінця сторінки, показуємо елемент "gotop-wrapper"
   if (currentPosition + windowHeight >= pageHeight - 140) {
     goToTopWrapper.classList.add('show');
   } else {
