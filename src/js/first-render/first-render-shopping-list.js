15763809534c52327a2fad097f5d0ac51d7853ab
yurchykyur <yurchyk.yur.s@gmail.com>
2023-08-04 23:57:31 +0300

    update firstRenderShoppingList

--- src/js/first-render/first-render-shopping-list.js
+++ src/js/first-render/first-render-shopping-list.js
@@ -1,8 +1,10 @@
 import createPagination from '../pagination/pagination'
+import getDataLocalStorage from '../service-local-storage/get-data-local-storage'
 
 function firstRenderShoppingList() {
     // функція запиту в локал сторедж та отриманні даних
-    const data = false; 
+    const data = getDataLocalStorage(key); 
+
 
     // Виклик функції створення розмітки відображення  книг
 
