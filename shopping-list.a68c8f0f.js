56567db79d0f9ef0d2fcd52bcbd3eac6bb0cd7e3
yurchykyur <124770215+yurchykyur@users.noreply.github.com>
2023-08-04 09:09:43 +0000

    Deploying to gh-pages from @ yurchykyur/project-stephen-js@99bfef5774c20327823e169b2aa54f3928fbea3c 🚀

--- shopping-list.a68c8f0f.js
+++
@@ -1,2 +0,0 @@
-!function(){let e=document.querySelector(".gotop-wrapper");document.querySelector(".gototop"),window.addEventListener("scroll",()=>{let o=document.documentElement.scrollTop,t=document.documentElement.scrollHeight,n=window.innerHeight;o+n>=t-140?e.classList.add("show"):e.classList.remove("show")})}();
-//# sourceMappingURL=shopping-list.a68c8f0f.js.map
--- shopping-list.c6f6cd26.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".gotop-wrapper");document.querySelector(".gototop"),window.addEventListener("scroll",()=>{let o=document.documentElement.scrollTop,t=document.documentElement.scrollHeight,n=window.innerHeight;o+n>=t-140?e.classList.add("show"):e.classList.remove("show")});
-//# sourceMappingURL=shopping-list.c6f6cd26.js.map
