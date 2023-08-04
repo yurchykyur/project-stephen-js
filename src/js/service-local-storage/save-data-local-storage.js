644fc2f30cfaecf732e4a378f647cbe925eea4b6
yurchykyur <yurchyk.yur.s@gmail.com>
2023-08-04 23:50:05 +0300

    service function local storage

---
+++ src/js/service-local-storage/save-data-local-storage.js
@@ -0,0 +1,10 @@
+export default function saveDataLocalStorage(key, value) {
+    
+    try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+
+}
