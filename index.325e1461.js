668dc445eff502b2aa1934bc19b486d553dd9840
yurchykyur <124770215+yurchykyur@users.noreply.github.com>
2023-08-04 13:43:22 +0000

    Deploying to gh-pages from @ yurchykyur/project-stephen-js@9399e6d2a07979cad8f0df0593be0b7540e60d33 🚀

--- index.325e1461.js
+++
@@ -1,2 +0,0 @@
-!function(){let e=document.querySelector(".gotop-wrapper");document.querySelector(".gototop"),window.addEventListener("scroll",()=>{let o=document.documentElement.scrollTop,t=document.documentElement.scrollHeight,n=window.innerHeight;o+n>=t-140?e.classList.add("show"):e.classList.remove("show")})}();
-//# sourceMappingURL=index.325e1461.js.map
