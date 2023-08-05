d9c187e0b0ba455ed0992d2f1da075103833cdfc
Kilsor <kilsors@gmail.com>
2023-08-05 20:03:15 +0300

    add header, Mobile Menu, Theme Switch

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './js/index/gotop';
 import './js/index/modal';
+import './js/burger-menu/burger-menu';
--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,3 +1,4 @@
 import './js/index/gotop';
 import './js/index/modal';
 import './js/pagination/pagination';
+import './js/burger-menu/burger-menu';
