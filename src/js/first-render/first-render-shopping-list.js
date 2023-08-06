e598bcd9e49200a0534d05faeba38cfff43dd229
OlegBarabash <olegbarabash7@gmail.com>
2023-08-06 20:26:35 +0300

    markup-and-rendering

--- src/js/first-render/first-render-shopping-list.js
+++ src/js/first-render/first-render-shopping-list.js
@@ -1,10 +1,10 @@
 import createPagination from '../pagination/pagination';
+import renderCards from '../shopping-list/render-cards';
 import getDataLocalStorage from '../service-local-storage/get-data-local-storage';
 
 export default function firstRenderShoppingList() {
   // функція запиту в локал сторедж та отриманні даних
-  const data = getDataLocalStorage(key);
-
+  //const data = getDataLocalStorage(key);
   // виклик створення розмітки пагінації та розмітки
   // createPagination(data, 1, true)
 }
