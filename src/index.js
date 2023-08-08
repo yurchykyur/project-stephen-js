dd066d612c60fd271b9098fd753cc29c341ec584
yurchykyur <yurchyk.yur.s@gmail.com>
2023-08-08 18:58:57 +0300

    refactored first render home

--- src/index.js
+++ src/index.js
@@ -8,3 +8,41 @@ import './js/best-sellers/best-sellers';
 import './js/best-sellers/fetchTopBooks';
 import './js/index/categories';
 import './js/banner/support-ukraine';
+
+import serviceBookAPI from './js/book-api/service-book-api';
+import renderCategories from './js/index/categories'
+
+
+  const topBooksPromise = serviceBookAPI('topBooks').catch(error => {
+    console.error('Error fetching topBooks:', error);
+    return [];
+  });
+
+  const categoryListPromise = serviceBookAPI('categoryList').catch(error => {
+    console.error('Error fetching categoryList:', error);
+    return [];
+  });
+
+  Promise.allSettled([topBooksPromise, categoryListPromise])
+    .then(results => {
+      const topBooksResult = results[0];
+      const categoryListResult = results[1];
+
+      if (topBooksResult.status === 'fulfilled') {
+        console.log('Top Books:', topBooksResult.value);
+        // Виклик функції для рендеру секції Мирослави
+      }
+
+        if (categoryListResult.status === 'fulfilled') {
+        renderCategories(categoryListResult.value)
+
+        console.log('Category List:', categoryListResult.value);
+        // Виклик функції для рендеру секції Ростислава
+      }
+    })
+    .finally(() => {
+      console.log('Both requests are settled.');
+      // Додатковий код, який буде виконано незалежно від результатів запитів
+    });
+
+
