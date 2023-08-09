9739b5bc9dc19b1130b1195498603bdd2b4f01b2
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-08-09 21:51:48 +0100

    changers

--- src/js/best-sellers/best-sellers.js
+++ src/js/best-sellers/best-sellers.js
@@ -11,7 +11,6 @@ const titleBooksList = document.querySelector('.books-list-title');
 
 let limit = 1;
 let DATA = [];
-// let isShow = 0;
 
 const title = document.querySelector('title');
 if (title.text == 'Bookshelf') {
@@ -19,7 +18,6 @@ if (title.text == 'Bookshelf') {
 }   
 
 export function onRenderBestsellers() {
-    // fetchTopBooks().then(dataBestsellers).catch();
   fetchTopBooks().then((data)=> dataBestsellers(data, true)).catch();
 }
 
@@ -70,28 +68,10 @@ window.addEventListener("resize", () => {
 function dataBestsellers(data, isFirstRender = false) {
   LocalStorageManager.saveData(LS_KEY, data);
   console.log('dataBestsellers')
-    // resizeLimit();
     if (isFirstRender) {
      DATA = [...data]
     onScreenController(window.innerWidth)
   }
-
-// function dataBestsellers(data) {
-//     resizeLimit();
-
-    // function resizeLimit() {
-    //     const vw = Math.max(
-    //         document.documentElement.clientWidth || 0,
-    //         window.innerWidth || 0
-    //     );
-    //     if (vw < 768) {
-    //         limit = 1;
-    //     } else if (vw < 1440) {
-    //         limit = 3;
-    //     } else {
-    //         limit = 5;
-    //     }
-    // }
   
     ulBooksList.innerHTML = '';
     const dataBestsellers = data
@@ -131,52 +111,13 @@ function dataBestsellers(data, isFirstRender = false) {
         })
     .join(' ');
   
-  
-  
-
     ulBooksListTop.innerHTML = dataBestsellers;
-
     const dataMarkupTitle = `<h2>Best Sellers <span class="colortext">Books</span></h2>`;
     divBooksList.innerHTML = dataMarkupTitle;
     onCategorriesBtn();
-
 }
 
-// isShow = 0;
-
-// async function fetchTopBooks(book) {
-//   console.log('fetchTopBooks');
-//   categorriesBtn.classList.add('is-hidden');
-//   const result = await fetch.fetchTopBooks(book);
-//   const { hits, total } = result;
-//   isShow += hits.length;
-
-//   if (!hits.length) {
-//     Notify.failure(
-//       `Sorry, there are no images matching your search query. Please try adain.`
-//     );
-//     categorriesBtn.classList.add('is-hidden');
-//     return;
-//   }
-
-//   onRenderBestsellers(hits);
-//   isShow += hits.length;
-
-//   if (isShow < total) {
-//     Notify.success(`Hooray! We found ${total} images !!!`);
-//     categorriesBtn.classList.remove('is-hidden');
-//   }
-
-//   if (isShow >= total) {
-//     Notify.info("We're sorry, but you've reached the end of search results.");
-//   }
-// }
-
-
-
-
 function onCategorriesBtn() {
-    // console.log('onCategorriesBtn');
     const categorriesBtn = document.querySelectorAll('.best-sellers-btn');
     categorriesBtn.forEach(element =>
         element.addEventListener('click', onFiltred));
@@ -190,29 +131,9 @@ function onLoader() {
 function onFiltred(event) {
     event.preventDefault();
 
-    // if (event.target.tagName !== 'LI' && event.target.tagName !== 'BUTTON')
-    //     return;
-
     let cateroryName = event.target.dataset['filter'];
     let cateroryNamePart = cateroryName.split(' ').slice(0, -1).join(' ');
     let lastWord = cateroryName.split(' ').pop();
-
-    // const dataMarkupTitle = `<h2>${cateroryNamePart} <span>${lastWord}</span></h2>`;
-    // titleBooksList.innerHTML = dataMarkupTitle;
-
-    // removeActiveClass();
-
-    // event.target.classList.add('active');
-    // function removeActiveClass() {
-    //     const listNames = document.querySelectorAll('.categories-list-name');
-    //     listNames.forEach(elem => {
-    //         if (elem.textContent === cateroryName) {
-    //             elem.classList.add('active');
-    //         } else {
-    //             elem.classList.remove('active');
-    //         }
-    //     });
-    // }
 
     if (cateroryName === 'Best Sellers Books') {
         onRenderBestsellers();
@@ -220,18 +141,12 @@ function onFiltred(event) {
     }
     onLoader();
     console.log(cateroryName); 
-  // console.log(dataMarkup);
-  // const parent = event.target.closest('.js-category-top-books');
-  // const id = parent;
-  // console.dir(id);
+
   const getDataLS = LocalStorageManager.getData(LS_KEY);
   creatMarkup(getDataLS, cateroryName);
-//   fetchBooks(cateroryName).then((data) => dataMarkup(data, cateroryName)).catch();
 }
 
 function dataMarkup(booksData, dataAttr) {
-  // console.log('fghhjk');
-    // ulBooksListTop.innerHTML = '';
     if (booksData.length === 0) {
         console.log('Немає інфо');
 
@@ -264,11 +179,8 @@ function dataMarkup(booksData, dataAttr) {
         })
         .join('');
     
-  // ulBooksList.innerHTML = dataMarkup;
  
   document.querySelector(`[data-filter="${dataAttr}"]`).innerHTML = dataMarkup;
-    // offLoader();
-  // console.log(dataMarkup);
   console.log("document.querySelector('.category-top-books", document.querySelector(`[data-filter="${dataAttr}"]`));
 }
 
@@ -326,12 +238,6 @@ function fetchBooks(cateroryName) {
     return response.json();
   });  
 }
-
-// function offLoader() {
-//   console.log('mask');
-//   mask.classList.remove('visible');
-// }
-
 
 document.querySelector('.books-list-top').addEventListener('click', onClickBook)
 function onClickBook(e) {
