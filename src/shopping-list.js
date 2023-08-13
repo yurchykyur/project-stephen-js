e125dc07b4af0e0a7c2551e69e2d9d4a8393b901
yurchykyur <yurchyk.yur.s@gmail.com>
2023-08-13 11:14:46 +0300

    pagination

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -6,7 +6,7 @@ import './js/header/anti-flicker.js';
 import './js/header/header.js';
 import './js/banner/support-ukraine';
 
-import createPagination from './js/pagination/pagination';
+import createPagination from './js/shopping-list/pagination';
 import LocalStorageManager from './js/service-local-storage/local-storage-manager';
 
 const SHOPPING_LIST_LS = 'shopping list';
